8f0572e4acfad2eeb5ca634945702f93eb9da935
Matheus Mikio <matheusmikio15@gmail.com>
2026-04-24 10:19:47 -0300

    Add RoleUser type and roles to user schema

--- internal/domain/schemas/user.go
+++ internal/domain/schemas/user.go
@@ -1,4 +1,13 @@
 package schemas
 
+type RoleUser string
+
+const(
+  Admin RoleUser = "ADMIN"
+  Coordinator RoleUser = "COORDENADOR"
+  Reviewer RoleUser = "REVISOR"
+)
+
 type User struct {
+  Role RoleUser `gorm:"type:role_user;not null;default:'REVISOR'"`
 }
