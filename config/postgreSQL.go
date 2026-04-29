cfe895f868cf9d24334f80293935ae7b6d4a7398
Matheus Mikio <matheusmikio15@gmail.com>
2026-04-29 20:05:34 -0300

    Refactory \config

--- config/postgreSQL.go
+++ config/postgreSQL.go
@@ -2,24 +2,26 @@ package config
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func initPostGreSQL() (*gorm.DB, error) {
-	_ = godotenv.Load()
-	_ = godotenv.Load(filepath.Join("..", "..", ".env"))
+func initPostGreSQL(cfg Config) (*gorm.DB, error) {
 
-	dbConnection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sskmode=disabled",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	dbConnection := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+		cfg.DBSSLMode,
+	)
 
 	db, err := gorm.Open(postgres.Open(dbConnection), &gorm.Config{})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("open postgres connection: %w", err)
 	}
 
 	// USAR EM DEV
